e11269f1860fe06fd084d1616a8ba702de67a676
fabrriva <fabrice.riva@dayonefactory.ch>
2021-01-03 20:30:22 +0100

    responsive slides

--- assets/js/slider/index.js
+++ assets/js/slider/index.js
@@ -7,9 +7,6 @@ require('flickity-fullscreen');
 import 'flickity/css/flickity.css';
 import 'flickity-fullscreen/fullscreen.css';
 
-//Slider style
-import '../../styles/slider/slider.scss';
-
 const base_options = {
     imagesLoaded: true,
     lazyLoad: true,
